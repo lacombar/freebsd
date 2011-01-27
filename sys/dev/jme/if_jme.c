lacombar/freebsd

commit 0f20b333d45a9ea56505f2f308a85f9519e6e181

Convert the PHY drivers to honor the mii_flags passed down and convert the NIC drivers as well as the PHY drivers to take advantage of the mii_attach() introduced in r213878 to get rid of certain hacks. For the most part these were: - Artificially limiting miibus_{read,write}reg methods to certain PHY   addresses; we now let mii_attach() only probe the PHY at the desired   address(es) instead. - PHY drivers setting MIIF_* flags based on the NIC driver they hang   off from, partly even based on grabbing and using the softc of the   parent; we now pass these flags down from the NIC to the PHY drivers   via mii_attach(). This got us rid of all such hacks except those of   brgphy() in combination with bce(4) and bge(4), which is way beyond   what can be expressed with simple flags.
While at it, I took the opportunity to change the NIC drivers to pass
up the error returned by mii_attach() (previously by mii_phy_probe())
and unify the error message used in this case where and as appropriate
as mii_attach() actually can fail for a number of reasons, not just
because of no PHY(s) being present at the expected address(es).

Reviewed by:	jhb, yongari


git-svn-id: svn://svn.freebsd.org/base/head@213893 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/dev/jme/if_jme.c
@@ -224,13 +224,8 @@ jme_miibus_readreg(device_t dev, int phy, int reg)
 	sc = device_get_softc(dev);
 
 	/* For FPGA version, PHY address 0 should be ignored. */
-	if ((sc->jme_flags & JME_FLAG_FPGA) != 0) {
-		if (phy == 0)
-			return (0);
-	} else {
-		if (sc->jme_phyaddr != phy)
-			return (0);
-	}
+	if ((sc->jme_flags & JME_FLAG_FPGA) != 0 && phy == 0)
+		return (0);
 
 	CSR_WRITE_4(sc, JME_SMI, SMI_OP_READ | SMI_OP_EXECUTE |
 	    SMI_PHY_ADDR(phy) | SMI_REG_ADDR(reg));
@@ -260,13 +255,8 @@ jme_miibus_writereg(device_t dev, int phy, int reg, int val)
 	sc = device_get_softc(dev);
 
 	/* For FPGA version, PHY address 0 should be ignored. */
-	if ((sc->jme_flags & JME_FLAG_FPGA) != 0) {
-		if (phy == 0)
-			return (0);
-	} else {
-		if (sc->jme_phyaddr != phy)
-			return (0);
-	}
+	if ((sc->jme_flags & JME_FLAG_FPGA) != 0 && phy == 0)
+		return (0);
 
 	CSR_WRITE_4(sc, JME_SMI, SMI_OP_WRITE | SMI_OP_EXECUTE |
 	    ((val << SMI_DATA_SHIFT) & SMI_DATA_MASK) |
@@ -743,9 +733,11 @@ jme_attach(device_t dev)
 	ifp->if_capenable = ifp->if_capabilities;
 
 	/* Set up MII bus. */
-	if ((error = mii_phy_probe(dev, &sc->jme_miibus, jme_mediachange,
-	    jme_mediastatus)) != 0) {
-		device_printf(dev, "no PHY found!\n");
+	error = mii_attach(dev, &sc->jme_miibus, ifp, jme_mediachange,
+	    jme_mediastatus, BMSR_DEFCAPMASK, sc->jme_phyaddr, MII_OFFSET_ANY,
+	    0);
+	if (error != 0) {
+		device_printf(dev, "attaching PHYs failed\n");
 		goto fail;
 	}
 

commit 00323684eb3dc4dda9aab4e81917d26dd2d113ab

MFC r216546,216548-216550: r216546:   Consistently put a tab character between #define and the macro name.
r216548:
  Fix a regression introduced in r213893. FPGA version requires PHY
  probing so allow PHY probing on all possible addresses.

r216549:
  Make sure whether driver allocated resource before releasing it.

r216550:
  Use system defined PCIR_EXPRESS_DEVICE_CTL instead of using magic
  number.

sys/dev/jme/if_jme.c
@@ -683,7 +683,7 @@ jme_attach(device_t dev)
 	/* Set max allowable DMA size. */
 	if (pci_find_extcap(dev, PCIY_EXPRESS, &i) == 0) {
 		sc->jme_flags |= JME_FLAG_PCIE;
-		burst = pci_read_config(dev, i + 0x08, 2);
+		burst = pci_read_config(dev, i + PCIR_EXPRESS_DEVICE_CTL, 2);
 		if (bootverbose) {
 			device_printf(dev, "Read request size : %d bytes.\n",
 			    128 << ((burst >> 12) & 0x07));
@@ -738,8 +738,9 @@ jme_attach(device_t dev)
 
 	/* Set up MII bus. */
 	error = mii_attach(dev, &sc->jme_miibus, ifp, jme_mediachange,
-	    jme_mediastatus, BMSR_DEFCAPMASK, sc->jme_phyaddr, MII_OFFSET_ANY,
-	    MIIF_DOPAUSE);
+	    jme_mediastatus, BMSR_DEFCAPMASK,
+	    sc->jme_flags & JME_FLAG_FPGA ? MII_PHY_ANY : sc->jme_phyaddr,
+	    MII_OFFSET_ANY, MIIF_DOPAUSE);
 	if (error != 0) {
 		device_printf(dev, "attaching PHYs failed\n");
 		goto fail;
@@ -861,10 +862,12 @@ jme_detach(device_t dev)
 		}
 	}
 
-	bus_release_resources(dev, sc->jme_irq_spec, sc->jme_irq);
+	if (sc->jme_irq[0] != NULL)
+		bus_release_resources(dev, sc->jme_irq_spec, sc->jme_irq);
 	if ((sc->jme_flags & (JME_FLAG_MSIX | JME_FLAG_MSI)) != 0)
 		pci_release_msi(dev);
-	bus_release_resources(dev, sc->jme_res_spec, sc->jme_res);
+	if (sc->jme_res[0] != NULL)
+		bus_release_resources(dev, sc->jme_res_spec, sc->jme_res);
 	mtx_destroy(&sc->jme_mtx);
 
 	return (0);

sys/dev/jme/if_jmevar.h
@@ -66,8 +66,8 @@
  * JMC250 can send 9K jumbo frame on Tx path and can receive
  * 65535 bytes.
  */
-#define JME_JUMBO_FRAMELEN	9216
-#define JME_JUMBO_MTU							\
+#define	JME_JUMBO_FRAMELEN	9216
+#define	JME_JUMBO_MTU							\
 	(JME_JUMBO_FRAMELEN - sizeof(struct ether_vlan_header) -	\
 	 ETHER_HDR_LEN - ETHER_CRC_LEN)
 #define	JME_MAX_MTU							\
@@ -143,14 +143,14 @@ struct jme_ring_data {
 	bus_addr_t		jme_ssb_block_paddr;
 };
 
-#define JME_TX_RING_ADDR(sc, i)	\
+#define	JME_TX_RING_ADDR(sc, i)	\
     ((sc)->jme_rdata.jme_tx_ring_paddr + sizeof(struct jme_desc) * (i))
-#define JME_RX_RING_ADDR(sc, i)	\
+#define	JME_RX_RING_ADDR(sc, i)	\
     ((sc)->jme_rdata.jme_rx_ring_paddr + sizeof(struct jme_desc) * (i))
 
-#define JME_TX_RING_SIZE	\
+#define	JME_TX_RING_SIZE	\
     (sizeof(struct jme_desc) * JME_TX_RING_CNT)
-#define JME_RX_RING_SIZE	\
+#define	JME_RX_RING_SIZE	\
     (sizeof(struct jme_desc) * JME_RX_RING_CNT)
 #define	JME_SSB_SIZE		sizeof(struct jme_ssb)
 
@@ -223,14 +223,14 @@ struct jme_softc {
 };
 
 /* Register access macros. */
-#define CSR_WRITE_4(_sc, reg, val)	\
+#define	CSR_WRITE_4(_sc, reg, val)	\
 	bus_write_4((_sc)->jme_res[0], (reg), (val))
-#define CSR_READ_4(_sc, reg)		\
+#define	CSR_READ_4(_sc, reg)		\
 	bus_read_4((_sc)->jme_res[0], (reg))
 
-#define JME_LOCK(_sc)		mtx_lock(&(_sc)->jme_mtx)
-#define JME_UNLOCK(_sc)		mtx_unlock(&(_sc)->jme_mtx)
-#define JME_LOCK_ASSERT(_sc)	mtx_assert(&(_sc)->jme_mtx, MA_OWNED)
+#define	JME_LOCK(_sc)		mtx_lock(&(_sc)->jme_mtx)
+#define	JME_UNLOCK(_sc)		mtx_unlock(&(_sc)->jme_mtx)
+#define	JME_LOCK_ASSERT(_sc)	mtx_assert(&(_sc)->jme_mtx, MA_OWNED)
 
 #define	JME_MAXERR	5
 
@@ -242,8 +242,8 @@ do {									\
 } while (0)
 
 #define	JME_TX_TIMEOUT		5
-#define JME_TIMEOUT		1000
-#define JME_PHY_TIMEOUT		1000
-#define JME_EEPROM_TIMEOUT	1000
+#define	JME_TIMEOUT		1000
+#define	JME_PHY_TIMEOUT		1000
+#define	JME_EEPROM_TIMEOUT	1000
 
 #endif